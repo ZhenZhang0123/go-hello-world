3fe3688811ede97db5073dadad728217cf0cfa18
zhenzhang0123 <zhen.zhang0123@gmail.com>
2024-06-03 19:35:58 -0400

    generics phase1

--- hello/hello.go
+++ hello/hello.go
@@ -29,4 +29,38 @@ func main() {
     
     fmt.Println(reverse.String("Hello"))
     fmt.Println(reverse.String("Hello"), reverse.Int(24601))
+
+    // Initialize a map for the integer values
+    ints := map[string]int64{
+        "first":  34,
+        "second": 12,
+    }
+
+    // Initialize a map for the float values
+    floats := map[string]float64{
+        "first":  35.98,
+        "second": 26.99,
+    }
+
+    fmt.Printf("Non-Generic Sums: %v and %v\n",
+        SumInts(ints),
+        SumFloats(floats))
+}
+
+// SumInts adds together the values of m.
+func SumInts(m map[string]int64) int64 {
+    var s int64
+    for _, v := range m {
+        s += v
+    }
+    return s
+}
+
+// SumFloats adds together the values of m.
+func SumFloats(m map[string]float64) float64 {
+    var s float64
+    for _, v := range m {
+        s += v
+    }
+    return s
 }
