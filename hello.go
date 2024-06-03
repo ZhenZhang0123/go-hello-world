0a504fa54df9907b5331f6593a9166a4f04ebb43
zhenzhang0123 <zhen.zhang0123@gmail.com>
2024-06-03 14:51:52 -0400

    call from another module

--- hello.go
+++ hello.go
@@ -1,9 +1,15 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+
+    "example/greetings"
+)
 
 import "rsc.io/quote"
 
 func main() {
     fmt.Println(quote.Go())
+	message := greetings.Hello("Gladys")
+    fmt.Println(message)
 }
