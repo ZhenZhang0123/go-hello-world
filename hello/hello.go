1be1163abbba4f989d11588f189cef080e6766b1
zhenzhang0123 <zhen.zhang0123@gmail.com>
2024-06-03 20:24:02 -0400

    generics

--- hello/hello.go
+++ hello/hello.go
@@ -1,66 +1,102 @@
 package main
 
 import (
-    "fmt"
-    "log"
+	"fmt"
+	"log"
 
-    "example.com/greetings"
-    "golang.org/x/example/hello/reverse"
+	"example.com/greetings"
+	"golang.org/x/example/hello/reverse"
 )
 
+type Number interface {
+	int64 | float64
+}
+
 func main() {
-    // Set properties of the predefined Logger, including
-    // the log entry prefix and a flag to disable printing
-    // the time, source file, and line number.
-    log.SetPrefix("greetings: ")
-    log.SetFlags(0)
+	// Set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
 
-    // A slice of names.
-    names := []string{"Gladys", "Samantha", "Darrin"}
+	// A slice of names.
+	names := []string{"Gladys", "Samantha", "Darrin"}
 
-    // Request greeting messages for the names.
-    messages, err := greetings.Hellos(names)
-    if err != nil {
-        log.Fatal(err)
-    }
-    // If no error was returned, print the returned map of
-    // messages to the console.
-    fmt.Println(messages)
-    
-    fmt.Println(reverse.String("Hello"))
-    fmt.Println(reverse.String("Hello"), reverse.Int(24601))
+	// Request greeting messages for the names.
+	messages, err := greetings.Hellos(names)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// If no error was returned, print the returned map of
+	// messages to the console.
+	fmt.Println(messages)
 
-    // Initialize a map for the integer values
-    ints := map[string]int64{
-        "first":  34,
-        "second": 12,
-    }
+	fmt.Println(reverse.String("Hello"))
+	fmt.Println(reverse.String("Hello"), reverse.Int(24601))
 
-    // Initialize a map for the float values
-    floats := map[string]float64{
-        "first":  35.98,
-        "second": 26.99,
-    }
+	// Initialize a map for the integer values
+	ints := map[string]int64{
+		"first":  34,
+		"second": 12,
+	}
 
-    fmt.Printf("Non-Generic Sums: %v and %v\n",
-        SumInts(ints),
-        SumFloats(floats))
+	// Initialize a map for the float values
+	floats := map[string]float64{
+		"first":  35.98,
+		"second": 26.99,
+	}
+
+	fmt.Printf("Non-Generic Sums: %v and %v\n",
+		SumInts(ints),
+		SumFloats(floats))
+
+	fmt.Printf("Generic Sums: %v and %v\n",
+		SumIntsOrFloats[string, int64](ints),
+		SumIntsOrFloats[string, float64](floats))
+
+	fmt.Printf("Generic Sums, type parameters inferred: %v and %v\n",
+		SumIntsOrFloats(ints),
+		SumIntsOrFloats(floats))
+
+	fmt.Printf("Generic Sums with Constraint: %v and %v\n",
+		SumNumbers(ints),
+		SumNumbers(floats))
 }
 
 // SumInts adds together the values of m.
 func SumInts(m map[string]int64) int64 {
-    var s int64
-    for _, v := range m {
-        s += v
-    }
-    return s
+	var s int64
+	for _, v := range m {
+		s += v
+	}
+	return s
 }
 
 // SumFloats adds together the values of m.
 func SumFloats(m map[string]float64) float64 {
-    var s float64
-    for _, v := range m {
-        s += v
-    }
-    return s
+	var s float64
+	for _, v := range m {
+		s += v
+	}
+	return s
+}
+
+// SumIntsOrFloats sums the values of map m. It supports both int64 and float64
+// as types for map values.
+func SumIntsOrFloats[K comparable, V int64 | float64](m map[K]V) V {
+	var s V
+	for _, v := range m {
+		s += v
+	}
+	return s
+}
+
+// SumNumbers sums the values of map m. It supports both integers
+// and floats as map values.
+func SumNumbers[K comparable, V Number](m map[K]V) V {
+	var s V
+	for _, v := range m {
+		s += v
+	}
+	return s
 }
