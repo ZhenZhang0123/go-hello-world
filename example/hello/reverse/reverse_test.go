84dcd98d0c2abd8e13a6be30090a2ee3ff88c489
zhenzhang0123 <zhen.zhang0123@gmail.com>
2024-06-03 21:32:34 -0400

    git rm --cached example

---
+++ example/hello/reverse/reverse_test.go
@@ -0,0 +1,42 @@
+// Copyright 2023 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package reverse
+
+import (
+	"testing"
+	"unicode/utf8"
+)
+
+func TestString(t *testing.T) {
+	for _, c := range []struct {
+		in, want string
+	}{
+		{"Hello, world", "dlrow ,olleH"},
+		{"Hello, 世界", "界世 ,olleH"},
+		{"", ""},
+	} {
+		got := String(c.in)
+		if got != c.want {
+			t.Errorf("String(%q) == %q, want %q", c.in, got, c.want)
+		}
+	}
+}
+
+func FuzzReverse(f *testing.F) {
+    testcases := []string{"Hello, world", " ", "!12345"}
+    for _, tc := range testcases {
+        f.Add(tc)  // Use f.Add to provide a seed corpus
+    }
+    f.Fuzz(func(t *testing.T, orig string) {
+        rev := String(orig)
+        doubleRev := String(rev)
+        if orig != doubleRev {
+            t.Errorf("Before: %q, after: %q", orig, doubleRev)
+        }
+        if utf8.ValidString(orig) && !utf8.ValidString(rev) {
+            t.Errorf("Reverse produced invalid UTF-8 string %q", rev)
+        }
+    })
+}
